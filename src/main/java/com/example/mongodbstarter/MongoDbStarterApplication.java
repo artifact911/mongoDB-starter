3e9f0b350d040342481fa1044e5f1e2e0bd53e05
KorbenDallas <anurnazarov@alfabank.ru>
2024-05-03 14:17:44 +0300

    first commit

---
+++ src/main/java/com/example/mongodbstarter/MongoDbStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.mongodbstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MongoDbStarterApplication.class, args);
+    }
+
+}
